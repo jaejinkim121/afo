b8ee92f5a61915047e1488bc98fb385f21bb9f20
jaejinkim121 <gisgen9@snu.ac.kr>
2022-08-31 16:34:07 +0900

    Add serial data initialization

--- afo_sensor/src/serial.cpp
+++ afo_sensor/src/serial.cpp
@@ -30,6 +30,10 @@ vector<string> split_comma(uint8_t *incomingData, char delimiter) {
 
 serial::serial(const char *device, const int baud) {
 	this->serialOpen(device, baud);
+	this->sole = {1, 1, 1, 1, 1, 1};
+	for (int i = 0; i < 63 ; i++){
+		this->imuData[i] = 0;
+	}
 }
 
 serial::~serial() {
