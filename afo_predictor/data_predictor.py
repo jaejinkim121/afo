4c1ba70995b28bf096083606451314c3807fb3f1
jaejinkim121 <gisgen9@snu.ac.kr>
2022-09-01 13:57:49 +0900

    Move macre to top of code

--- afo_predictor/data_predictor.py
+++ afo_predictor/data_predictor.py
@@ -1,10 +1,10 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Sun Aug 21 01:38:40 2022
 
 @author: minhee
 """
-#!/usr/bin/env python3
 import os
 import numpy as np
 import pandas as pd
