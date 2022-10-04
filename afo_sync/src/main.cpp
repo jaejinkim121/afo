78e6ca585032b4f7b36552e3326f79bf52d4734d
jaejinkim121 <gisgen0@snu.ac.kr>
2022-10-04 11:43:15 +0900

    Sync thread setup

--- afo_sync/src/main.cpp
+++ afo_sync/src/main.cpp
@@ -1,43 +1,62 @@
 #include <iostream>
 #include "ros/ros.h"
 #include "std_msgs/Bool.h"
+#include <thread>
+#include <limits>
 
 using namespace std;
+
+bool endSign = false;
+int toggle(bool* current_sync, bool* current_zero){
+	int _tmp;
+	while(!endSign){
+		cout << "Toggle Sync signal: ";
+		scanf("%d", &_tmp);
+		if (_tmp == 1){
+			*current_zero = !(*current_zero);
+			cout << "afo_sync Node - msg 'zero' published, value = " << *current_zero << endl;
+		}
+		else{
+			*current_sync = !(*current_sync);
+			cout << "afo_sync Node - msg 'sync' published, value = " << *current_sync << endl;
+		}
+	}
+	return 1;
+}
 
 int main(int argc, char** argv){
     ros::init(argc, argv, "afo_sync");
 	ros::NodeHandle n;
-int rr;
-n.getParam("/rr", rr);
-    ros::Rate loop_rate(rr);
-    ros::Publisher afo_sync_pub = n.advertise<std_msgs::Bool>("/afo_sync/sync", 100);
-    ros::Publisher afo_zero_pub = n.advertise<std_msgs::Bool>("/afo_sync/zero", 100);
+	int rr;
+    	ros::Publisher afo_sync_pub = n.advertise<std_msgs::Bool>("/afo_sync/sync", 100);
+    	ros::Publisher afo_zero_pub = n.advertise<std_msgs::Bool>("/afo_sync/zero", 100);
 
-    std_msgs::Bool msg_sync;
-    std_msgs::Bool msg_zero;
+	std_msgs::Bool msg_sync;
+    	std_msgs::Bool msg_zero;
 
-    bool current_sync = false;
-    bool current_zero = false;
-    msg_sync.data = current_sync;
-    afo_sync_pub.publish(msg_sync);
-char _tmp;
+    	bool* current_sync = new bool;
+    	bool* current_zero = new bool;
+    	*current_sync = false;
+    	*current_zero = false;
+
+	int _tmp;
     while(ros::ok()){
-	cout << "Toggle Sync signal: ";
-	cin >> _tmp;
-        if (_tmp == '0'){
-		current_zero = !current_zero;
-		msg_zero.data = current_zero;
-		afo_zero_pub.publish(msg_zero);
-		cout << "afo_sync Node - msg 'zero' published, value = " << current_zero <<endl;
-	}
-	else{
-		current_sync = !current_sync;
-		msg_sync.data = current_sync;
-		afo_sync_pub.publish(msg_sync);
-		cout << "afo_sync Node - msg 'sync' published, value = " << !current_sync << endl;
+cout << "Toggle Sync signal: ";
+cin >> _tmp;		
+if (_tmp == 1){
+			*current_zero = !(*current_zero);
+			cout << "afo_sync Node - msg 'zero' published, value = " << *current_zero << endl;
 		}
+		else{
+			*current_sync = !(*current_sync);
+			cout << "afo_sync Node - msg 'sync' published, value = " << *current_sync << endl;
+		}
+	msg_zero.data = *current_zero;
+	msg_sync.data = *current_sync;
+	afo_zero_pub.publish(msg_zero);
+	afo_sync_pub.publish(msg_sync);
 	}
 	cout << "afo_sync Node - ros end - main end" << endl;
-
+endSign = true;
     return 1;
 }
