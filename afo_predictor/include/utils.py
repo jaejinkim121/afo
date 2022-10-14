c7c7c6681f0a91ca03d637760ba3d146679412dc
minhee1224 <mlee.minheelee@gmail.com>
2022-10-14 10:30:46 +0900

    modify previous code for prediction replay code

--- afo_predictor/include/utils.py
+++ afo_predictor/include/utils.py
@@ -7,6 +7,8 @@ Created on Tue Aug 23 00:35:03 2022
 
 import glob
 import os
+import numpy as np
+import pandas as pd
 
 def folder_path_name(path, option=None, char=None, T_F=None):
 
@@ -45,3 +47,28 @@ def folder_path_name(path, option=None, char=None, T_F=None):
     exp_list = [file.replace('\\', '/') for file in exp_list]
 
     return exp_list, exp_name_list
+
+
+def replace_multiple_text(text, dic={",": " ", "  ": " "}):
+    for i, j in dic.items():
+        text = text.replace(i, j)
+    return text
+
+
+def vout_preprocessing(path):
+
+    sensor_csv = path
+
+    # Multiple delimiter
+    with open(sensor_csv) as data:
+        conv_data = [replace_multiple_text(line) for line in data]
+
+    sensor_data = np.loadtxt(conv_data, delimiter=" ", skiprows=1)
+    # sensor_vout_header = ["swing_phase", "sync", "time",
+    #                   "v1" ,"v2", "v3", "v4", "v5", "v6"]
+    sensor_force_header = ["swing_phase", "sync", "time",
+                      "f1" ,"f2", "f3", "f4", "f5", "f6"]
+
+    data_buffer = sensor_data[:,3:]
+    data_front = sensor_data[:,:3]
+    return data_buffer, data_front, sensor_force_header
