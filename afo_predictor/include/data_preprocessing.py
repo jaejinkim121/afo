eb3cbd21188c491cb459aea131f87440735161a4
minhee1224 <mlee.minheelee@gmail.com>
2022-10-18 18:57:47 +0900

    Modify path for data preprocessing file

--- afo_predictor/include/data_preprocessing.py
+++ afo_predictor/include/data_preprocessing.py
@@ -6,13 +6,8 @@ Created on Wed Aug 31 22:23:05 2022
 """
 
 import pandas as pd
-import os
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib as mpl
-import glob
-from scipy.signal import find_peaks
-from calib_function import *
+
+from utils import *
 
 NUM_PRE = 30
 WINDOWS = 30
@@ -22,7 +17,7 @@ df_sync = pd.DataFrame(columns=["folder_name", "pre_post", "time"])
 
 # read the folder name
 (exp_list, exp_name_list) = folder_path_name(
-    "./CHAR_0927_260/", "start", "Calibration", 1)
+    "../bin/calibration/CHAR_0927_260/", "start", "Calibration", 1)
 
 for (file, name) in zip(exp_list, exp_name_list):
 
@@ -38,50 +33,37 @@ for (file, name) in zip(exp_list, exp_name_list):
         # internal path - individual sensor
         int_name_path = str(test_list)+"/"
         (int_list, int_name_list) = folder_path_name(int_name_path)
-        # interpolation preprocessing
-        for (int_path, int_name) in zip(int_list, int_name_list):
-            # instron data
-            if int_name.startswith("interlink") == 1:
-                # instron data sync, preprocessing
-                sync_time = instron_interp_preprocessing(
-                    int_path, int_name_path + "instron_cycle300um_mod.csv")
-                df_sync = df_sync.append({"folder_name": name_list,
-                                          "pre_post": str(name),
-                                          "time": sync_time},
-                                         ignore_index=True)
-            # sensor data
-            elif int_name.startswith("pointSensor") == 1:
-                # sensor numbering from 1
-                num = int(sensor_num)
-                # sensor data preprocessing
-                # sensor_data = pd.read_csv(int_path, sep=" |,", header=1)
-                raspi_interp_preprocessing(int_path, num,
-                                           int_name_path +
-                                           "sensor_cycle300um_mod.csv")
+        # # interpolation preprocessing
+        # for (int_path, int_name) in zip(int_list, int_name_list):
+        #     # instron data
+        #     if int_name.startswith("interlink") == 1:
+        #         # instron data sync, preprocessing
+        #         sync_time = instron_interp_preprocessing(
+        #             int_path, int_name_path + "instron_cycle300um_mod.csv")
+        #         df_sync = df_sync.append({"folder_name": name_list,
+        #                                   "pre_post": str(name),
+        #                                   "time": sync_time},
+        #                                  ignore_index=True)
+        #     # sensor data
+        #     elif int_name.startswith("pointSensor") == 1:
+        #         # sensor numbering from 1
+        #         num = int(sensor_num)
+        #         # sensor data preprocessing
+        #         # sensor_data = pd.read_csv(int_path, sep=" |,", header=1)
+        #         raspi_interp_preprocessing(int_path, num,
+        #                                    int_name_path +
+        #                                    "sensor_cycle300um_mod.csv")
 
-        # interpolation
-        calib_interp(int_name_path + "instron_cycle300um_mod.csv",
-                     sync_time,
-                     int_name_path + "sensor_cycle300um_mod.csv",
-                     int_name_path + "force_conversion_test.csv")
+        # # interpolation
+        # calib_interp(int_name_path + "instron_cycle300um_mod.csv",
+        #              sync_time,
+        #              int_name_path + "sensor_cycle300um_mod.csv",
+        #              int_name_path + "force_conversion_test.csv")
 
-        # N data save directory
-        inner_save_directory = str(int_name_path)+"N_data_test.csv"
-
-        # individual sensor for loop - interpolation check & N_data
+        # individual sensor for loop - interpolation check 
         for (int_path, int_name) in zip(int_list, int_name_list):
             # interpolation result
             if int_name.endswith("conversion_test.csv"):
                 calib_result_plot(str(int_path),
                                   "260size", name_list+name)
                 print("check_%s" % name_list)
-
-                final_data = pd.read_csv(str(int_path), header=0)
-                final_data = final_data.astype(float)
-
-                data = pd.DataFrame()
-                data[["time", "vout", "force"]] = final_data[["time",
-                                                              "vout",
-                                                              "force"]]
-                # data = N_data_preprocessing(data, NUM_PRE, WINDOWS, tol)
-                # data.to_csv(inner_save_directory, sep=',', index=False)
--- afo_predictor/include/utils.py
+++ afo_predictor/include/utils.py
@@ -9,6 +9,7 @@ import glob
 import os
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def folder_path_name(path, option=None, char=None, T_F=None):
 
@@ -47,6 +48,120 @@ def folder_path_name(path, option=None, char=None, T_F=None):
     exp_list = [file.replace('\\', '/') for file in exp_list]
 
     return exp_list, exp_name_list
+
+
+def instron_interp_preprocessing(instron_path, save_path):
+
+    instron_csv = instron_path
+    instron_data = pd.read_csv(instron_csv, header=0)
+    instron_data = pd.DataFrame.drop(instron_data, index=0, axis=0)
+    instron_data.reset_index(drop=True, inplace=True)
+    instron_data = instron_data.astype(float)
+    ini_vol = instron_data.at[0, 'voltage_1']
+    sync = np.where(abs(instron_data["voltage_1"] - ini_vol) >= 0.03)
+    sync_index = sync[0][0]
+    sync_time = instron_data.at[sync_index, 'Time']
+    instron_data.to_csv(str(save_path), sep=",", index=False)
+
+    return sync_time
+
+
+def raspi_interp_preprocessing(raspi_path, sensor_num, save_path):
+
+    sensor_csv = raspi_path
+    sensor_data = pd.read_csv(sensor_csv, sep=" |,", header=1)
+    # sensor_data = sensor_data.astype(float)
+    sensor_data_ = sensor_data.iloc[:, [2, 4, int(sensor_num)+5]]
+
+    sensor_data = sensor_data_
+    sensor_data.columns = ['sync', 'time', 'vout']
+    ini_sync = sensor_data.at[0, 'sync']
+    sync = np.where(abs(sensor_data["sync"] - ini_sync) >= 1)
+    sync_index = sync[0]
+    sensor_data = sensor_data.iloc[sync_index, :]
+    sensor_data.reset_index(inplace=True, drop=True)
+    print(sensor_data)
+    ini_time = sensor_data.at[0, 'time']
+    sensor_index = list(sensor_data.index)
+    del_time = []
+    for j in sensor_index:
+        if j == 0:
+            del_time = np.append(del_time, sensor_data.at[j, 'time']-ini_time)
+        else:
+            del_time = np.append(del_time,
+                                 sensor_data.at[j, 'time']
+                                 - sensor_data.at[j-1, 'time'])
+    d_time = pd.DataFrame(del_time, columns=["flag"])
+    d_time = d_time[d_time["flag"] > 0.003]
+    sensor_time_index = list(d_time.index)[0]
+    sensor_data = sensor_data[sensor_data.index >= sensor_time_index]
+    sensor_data["time"] = sensor_data["time"] - ini_time
+
+    # only for CHAR_0927_260
+    # sensor_data["vout"] = sensor_data["vout"] * (0.625/0.5)
+    sensor_data.to_csv(str(save_path), sep=",", index=False)
+
+
+def calib_interp(instron_path, instron_sync_time, sensor_path, save_path):
+
+    instron_data = pd.read_csv(str(instron_path), header=0)
+    instron_data = instron_data.astype(float)
+    instron_synced = instron_data[(abs(instron_data["voltage_1"] -
+                                       instron_data.at[0, "voltage_1"]) > 3.0)
+                                  & (instron_data["Time"]
+                                     >= instron_sync_time)]
+    instron_synced["Time"] = instron_synced["Time"] - instron_sync_time
+    instron_synced.reset_index(drop=True, inplace=True)
+
+    sensor_data = pd.read_csv(str(sensor_path), header=0)
+    sensor_data = sensor_data.astype(float)
+    sensor_synced = sensor_data[abs(sensor_data["sync"] -
+                                    sensor_data.at[0, "sync"]) == 0]
+    sensor_synced.reset_index(drop=True, inplace=True)
+
+    vout_synced = pd.DataFrame(np.interp(instron_synced["Time"],
+                                         sensor_synced["time"],
+                                         sensor_synced["vout"]),
+                                columns=["vout"])
+    calib_synced = pd.concat([instron_synced["Time"],
+                              vout_synced, instron_synced["Force"]], axis=1)
+    calib_synced.columns = ["time", "vout", "force"]
+    calib_synced.to_csv(str(save_path), sep=",", index=False)
+
+
+def calib_result_plot(path, RH_num, folder_name):
+
+    data = pd.read_csv(str(path), delimiter=",", header=0)
+
+    plt.figure(figsize=(6, 8))
+    plt.plot(data.loc[:, "time"], data.loc[:, "vout"],
+             label='%s_%s' % (str(RH_num), str(folder_name)))
+    plt.title("time vs vout")
+    plt.xlabel("Time [s]")
+    plt.ylabel("vout [V]")
+    plt.legend(loc='best')
+    plt.tight_layout()
+    plt.show()
+
+    plt.figure(figsize=(6, 8))
+    plt.plot(data.loc[:, "time"], data.loc[:, "force"],
+             label='%s_%s' % (str(RH_num), str(folder_name)))
+    plt.title("time vs force")
+    plt.xlabel("Time [s]")
+    plt.ylabel("Force [N]")
+    plt.legend(loc='best')
+    plt.tight_layout()
+    plt.show()
+
+    plt.figure(figsize=(6, 8))
+    plt.plot(data.loc[:, "vout"], data.loc[:, "force"],
+             label='%s_%s' % (str(RH_num), str(folder_name)))
+    plt.title("vout vs force")
+    plt.ylabel("Force [N]")
+    plt.xlabel("vout [V]")
+    plt.legend(loc='best')
+    plt.tight_layout()
+    plt.show()
 
 
 def replace_multiple_text(text, dic={",": " ", "  ": " "}):
