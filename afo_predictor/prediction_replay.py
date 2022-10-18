f2a6796dfa54574c8d245b713f81cb3d53f0e2b7
minhee1224 <mlee.minheelee@gmail.com>
2022-10-18 17:45:49 +0900

    Add for loop for trial num, model, test index in DataPredictionReplay

--- afo_predictor/prediction_replay.py
+++ afo_predictor/prediction_replay.py
@@ -8,14 +8,7 @@ Created on Thu Sep  1 16:00:11 2022
 import os
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-from tqdm import tqdm
-
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torchvision import transforms
-from torch.utils.data import Dataset, DataLoader, random_split
 
 from include.model import *
 from include.utils import *
@@ -23,7 +16,7 @@ from include.utils import *
 
 class DataPredictionReplay:
     def __init__(self, data, model_name="LSTM",
-                 model_dir="./data/CHAR_1010_280/",
+                 model_dir="./bin/model/CHAR_1010_280/",
                  sensor_dir="Left", sensor_size="280",
                  input_length=15, sensor_num=6):
         self.total_data = data
@@ -33,11 +26,14 @@ class DataPredictionReplay:
         self.model_name = model_name
         self.model_path = model_dir
         self.input_length = input_length
-        self.device = torch.device('cpu')
+        self.device = torch.device('cuda') \
+            if torch.cuda.is_available else torch.device('cpu')
+        print("Using PyTorch version: {}, Device: {}".format(
+            torch.__version__, self.device))
         self.model_load()
         
     def data_indexing(self, idx):
-        if idx <= len(self.total_data)-2:
+        if idx <= len(self.total_data)-1:
             self.data = self.total_data[0:idx+1][:]
         else:
             # end condition
@@ -53,7 +49,7 @@ class DataPredictionReplay:
             else:
                 pass
             model.load_state_dict(torch.load(
-                self.model_path + self.model_name + "_model/" +
+                self.model_path + self.model_name + "/" +
                 self.sensor_size + self.sensor_dir + "_" +
                 str(num + 1) + ".pt",
                 map_location=self.device))
@@ -89,7 +85,7 @@ class DataPredictionReplay:
         self.data_indexing(idx)
         _, sensor_name_list = folder_path_name(
             self.model_path + self.model_name +
-            "_model/", "include", self.sensor_dir)
+            "/", "include", self.sensor_dir)
         sensor_name_list = [name for name in sensor_name_list if \
                             int(name[-4]) <= self.sensor_num]
         sorted_name_list = sorted(sensor_name_list, key=lambda x: int(x[-4]),
@@ -107,7 +103,7 @@ class DataPredictionReplay:
                     x = self.LSTMtransform(int(name[-4]))
                 else:
                     pass
-                output = np.append(output, model(x))
+                output = np.append(output, model(x).detach().cpu().numpy())
 
         output = np.expand_dims(output, axis=0)
 
@@ -117,83 +113,107 @@ class DataPredictionReplay:
 if __name__ == "__main__":
 
     # TRIAL NUMBER
-    trial_num = 14
-    # trial_num_list = [11, 12, 13, 14]
-    clinical_data_path = "../afo_sensor/log/data/RH-%s/sensor/" \
-        % str(trial_num).zfill(2)
-    prediction_path = "../afo_sensor/log/data/RH-%s/force/" \
-        % str(trial_num).zfill(2)
-    try:
-        if not os.path.exists(prediction_path):
-            os.makedirs(prediction_path)
-    except:
-        pass
-    # calibration path, sensor size, model_name
-    calibration_path = "CHAR_1010_280"
-    calibration_size = "280"
-    model_name = "LSTM"
+    trial_num_list = [11, 13, 14]
+    # Model name list
+    model_list = ["LSTM", "CNN"]
     # TEST INDEX
-    test_index = "main"
-    test_index_list = ["launch_test", "sensor_test", "sync", "stance"]
-   
-    ###########################################################################
-    ###########################################################################
-    for test_index in test_index_list:
+    test_index_list = ["main_", "main2_", "main3_", "main4_",
+                       "stance_", "sensor_test_"]
+    # Calibration path, sensor size
+    calib_dict = {"11": ["CHAR_0919_280", "280"],
+                  "12": ["CHAR_0927_260", "260"],
+                  "13": ["CHAR_1004_280", "280"],
+                  "14": ["CHAR_1010_280", "280"]}
 
-        # sensor path assignment
-        (sensor_path, _) = folder_path_name(clinical_data_path, "start",
-                                            test_index, 1)
-        left_sensor_path = [path for path in sensor_path 
-                            if path.endswith("Leftsole.csv")==1]
-        right_sensor_path = [path for path in sensor_path 
-                            if path.endswith("Rightsole.csv")==1]
-    
-        # left sensor replay
-        left_data, left_data_front, left_header = vout_preprocessing(
-            left_sensor_path[0])
-        left_predictor = DataPredictionReplay(left_data,
-                                              model_name = model_name,
-                                              model_dir="./data/"+calibration_path+"/",
-                                              sensor_size=calibration_size)
-        left_force_data = left_predictor.prediction(idx=0)
-        left_idx = 1
-        while len(left_predictor.total_data)-left_idx >= 1:
-            left_force_data = np.append(left_force_data,
-                                        left_predictor.prediction(idx=left_idx),
-                                        axis=0)
-            left_idx += 1
-    
-        # right sensor replay
-        right_data, right_data_front, right_header = vout_preprocessing(
-            right_sensor_path[0])
-        right_predictor = DataPredictionReplay(right_data,
-                                               model_name = model_name,
-                                               model_dir="./data/"+calibration_path+"/",
-                                               sensor_size=calibration_size,
-                                               sensor_dir="Right")
-        right_force_data = right_predictor.prediction(idx=0)
-        right_idx = 1
-        while len(right_predictor.total_data)-right_idx >= 1:
-            right_force_data = np.append(right_force_data,
-                                        right_predictor.prediction(idx=right_idx),
-                                        axis=0)
-            right_idx += 1
-    
-        # save sensor data csv files
-        left_data_front = pd.DataFrame(left_data_front)
-        left_force_data = pd.DataFrame(left_force_data)
-        left_force_data = pd.concat([left_data_front, left_force_data], axis=1)
-        left_force_data.columns = left_header
-        left_force_data.to_csv(
-            prediction_path+"%s_%s_%s_Leftsole.csv" % (
-                test_index, model_name, calibration_path),
-            sep=",", header=True, index=False)
-    
-        right_data_front = pd.DataFrame(right_data_front)
-        right_force_data = pd.DataFrame(right_force_data)
-        right_force_data = pd.concat([right_data_front, right_force_data], axis=1)
-        right_force_data.columns = right_header
-        right_force_data.to_csv(
-            prediction_path+"%s_%s_%s_Rightsole.csv" % (
-                test_index, model_name, calibration_path),
-            sep=",", header=True, index=False)
+    ###########################################################################
+    ###########################################################################
+    # for loop for model list
+    for model_name in model_list:
+        print("START %s model prediction!" % model_name)
+        # for loop for trial num
+        for trial_num in trial_num_list:
+            print("Current trial num: %s" % str(trial_num).zfill(2))
+            # for loop for test index
+            for test_index in test_index_list:
+                print("Current test index: %s" % test_index)
+
+                clinical_data_path = "./bin/sensor/RH-%s/" \
+                    % str(trial_num).zfill(2)
+                prediction_path = "./bin/prediction/RH-%s/%s/" \
+                    % (str(trial_num).zfill(2), model_name)
+                try:
+                    if not os.path.exists(prediction_path):
+                        os.makedirs(prediction_path)
+                except:
+                    pass
+
+                # calibration path, sensor size
+                calibration_path = calib_dict[str(trial_num).zfill(2)][0]
+                calibration_size = calib_dict[str(trial_num).zfill(2)][1]
+
+                # sensor path assignment
+                (sensor_path, _) = folder_path_name(
+                    clinical_data_path, "start", test_index, 1)
+                left_sensor_path = [path for path in sensor_path 
+                                    if path.endswith("Leftsole.csv")==1]
+                right_sensor_path = [path for path in sensor_path 
+                                    if path.endswith("Rightsole.csv")==1]
+
+                # Exception in test index
+                if len(left_sensor_path) != 1:
+                    pass
+                else:
+
+                    # left sensor replay
+                    left_data, left_data_front, left_header = vout_preprocessing(
+                        left_sensor_path[0])
+                    left_predictor = DataPredictionReplay(
+                        left_data,
+                        model_name = model_name,
+                        model_dir="./bin/model/"+calibration_path+"/",
+                        sensor_size=calibration_size)
+                    left_force_data = left_predictor.prediction(idx=0)
+                    left_idx = 1
+                    for _ in np.arange(1, len(left_predictor.total_data)):
+                        left_force_data = np.append(
+                            left_force_data,
+                            left_predictor.prediction(idx=left_idx),
+                            axis=0)
+                        left_idx += 1
+                
+                    # right sensor replay
+                    right_data, right_data_front, right_header = vout_preprocessing(
+                        right_sensor_path[0])
+                    right_predictor = DataPredictionReplay(
+                        right_data,
+                        model_name = model_name,
+                        model_dir="./bin/model/"+calibration_path+"/",
+                        sensor_size=calibration_size,
+                        sensor_dir="Right")
+                    right_force_data = right_predictor.prediction(idx=0)
+                    right_idx = 1
+                    for _ in np.arange(1, len(right_predictor.total_data)):
+                        right_force_data = np.append(
+                            right_force_data,
+                            right_predictor.prediction(idx=right_idx),
+                            axis=0)
+                        right_idx += 1
+                
+                    # save sensor data csv files
+                    left_data_front = pd.DataFrame(left_data_front)
+                    left_force_data = pd.DataFrame(left_force_data)
+                    left_force_data = pd.concat(
+                        [left_data_front, left_force_data], axis=1)
+                    left_force_data.columns = left_header
+                    left_force_data.to_csv(
+                        prediction_path+"%sLeftsole.csv" % (test_index),
+                        sep=",", header=True, index=False)
+                
+                    right_data_front = pd.DataFrame(right_data_front)
+                    right_force_data = pd.DataFrame(right_force_data)
+                    right_force_data = pd.concat(
+                        [right_data_front, right_force_data], axis=1)
+                    right_force_data.columns = right_header
+                    right_force_data.to_csv(
+                        prediction_path+"%sRightsole.csv" % (test_index),
+                        sep=",", header=True, index=False)
