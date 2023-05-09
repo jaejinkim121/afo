135227f3c88c520794131369a469f9992e0a245a
jaejinkim121 <gisgen0@snu.ac.kr>
2023-05-09 10:52:24 +0900

    Add exe shell script

---
+++ run_afo.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+function cleanup { 
+  echo "Cleaning up before exit..." 
+  # Perform cleanup actions here 
+  exit 1
+} 
+trap cleanup SIGINT
+source /opt/ros/melodic/setup.bash
+source /home/srbl/catkin_ws/devel/setup.bash
+
+roslaunch afo_launch afo_without_prediction.launch
+
