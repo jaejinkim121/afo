7d565f3348896df44773e7a89da16399af7fc502
jaejinkim121 <gisgen9@snu.ac.kr>
2025-02-28 15:41:44 +0900

    Add encoder

--- sketch_sep17a/sketch_sep17a.ino
+++ sketch_sep17a/sketch_sep17a.ino
@@ -1,4 +1,4 @@
-#define USE_USBCON
+ #define USE_USBCON
 #include <ros.h>
 #include <std_msgs/Int16MultiArray.h>
 #include <Thread.h>
@@ -8,9 +8,23 @@ ros::NodeHandle nh;
 bool flagSwitch = true;
 int pinIn = A0;
 int valA = 0;
-int valArray[20] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+int valArray[21] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 std_msgs::Int16MultiArray msg_val;
 ros::Publisher pub_val("/afo_arduino/analog_val", &msg_val);
+
+
+bool flagSwitch = true;
+
+volatile int lastEncoded = 0;
+volatile long encoderValue = 0;
+
+int encoderPin1 = 2;
+int encoderPin2 = 3;
+
+int lastencoderValue = 0;
+
+int lastMSB = 0;
+int lastLSB = 0;
 
 Thread receiveThread = Thread();
 Thread pubThread = Thread();
@@ -38,6 +52,22 @@ void pubMsg(){
   pub_val.publish(&msg_val);
   nh.spinOnce();
 }
+
+
+void updateEncoder(){
+  int MSB = digitalRead(encoderPin1); //MSB = most significant bit
+  int LSB = digitalRead(encoderPin2); //LSB = least significant bit
+
+  int encoded = (MSB << 1) |LSB; //converting the 2 pin value to single number
+  int sum  = (lastEncoded << 2) | encoded; //adding it to the previous encoded value
+
+  if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) encoderValue ++;
+  if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) encoderValue --;
+
+  lastEncoded = encoded; //store this value for next time
+  valArray[20] = -encoderValue * 360 / 2048 + 90;
+}
+
 
 void setup(){
   pinMode(pinIn, INPUT);
