54cdcde8e506fea83a29981e6085c1ee0a4f1f64
jaejinkim121 <gisgen9@snu.ac.kr>
2025-10-31 14:29:09 +0900

    Debug

--- afo_detector/include/segment.hpp
+++ afo_detector/include/segment.hpp
@@ -12,4 +12,18 @@
 
 class Optimizer;
 
-class ImuOptimizer;
+class ImuOptimizer{
+    
+    private:
+        Optimizer opt_;
+        SampleIMU zero_;
+        std::vector<sampleTLA> seg_;
+        std::queue<NormTLA> q_;
+        NormTLA mean_;
+        std::vector<double> tla_;
+        std::vector<double> result_opt_;
+        std::array<Matrix3d, 7> R_fromZero_;
+        std::array<double, 7> linkLength_;
+        bool isLeft_;
+        bool isSetZero_;
+};
--- afo_detector/src/segment.cpp
+++ afo_detector/src/segment.cpp
@@ -298,16 +298,4 @@ class ImuOptimizer {
         }
         
 
-    private:
-        Optimizer opt_;
-        SampleIMU zero_;
-        std::vector<sampleTLA> seg_;
-        std::queue<NormTLA> q_;
-        NormTLA mean_;
-        std::vector<double> tla_;
-        std::vector<double> result_opt_;
-        std::array<Matrix3d, 7> R_fromZero_;
-        std::array<double, 7> linkLength_;
-        bool isLeft_;
-        bool isSetZero_;
 };
