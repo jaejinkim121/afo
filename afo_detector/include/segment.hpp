b8627938625d9c52cedd8691240567c5e5a9505c
jaejinkim121 <gisgen9@snu.ac.kr>
2025-10-31 14:31:22 +0900

    Debug

--- afo_detector/include/segment.hpp
+++ afo_detector/include/segment.hpp
@@ -10,14 +10,58 @@
 #include <Eigen/Dense>
 #include <Eigen/Geometry>
 
-class Optimizer;
+
+struct SampleIMU{
+    float t;
+    std::array<float, 21> value;
+};
+
+struct SampleTLA{
+    double t;
+    double value;
+};
+
+struct NormTLA{
+float cycle_time;
+std::array<double, N+1> d;
+};
+
+class Optimizer{
+    private:
+        // Define Optimization problem parameters.
+        static const unsigned int N_t = 99; // dt = 1 / (N_t+1). For this case, dt=10ms.
+        double torque_upper_limit = 1.0; // Can be kHighSInf
+        double jerk_absolute_upper_limit = 0.05;
+        double torque_impulse_value = 0.1 * N_t;
+
+        // Define HiGHS Instances.
+        HighsLp lp_;
+        Highs highs_;
+        HighsStatus return_status_;
+
+        const int num_col = N_t;
+        const int num_row = N_t + 2;
+
+        // Define state-relevant variables.
+        std::vector<double> col_cost;
+        std::vector<double> col_lower;
+        std::vector<double> col_upper;
+        
+        // 제약 하한/상한
+        std::vector<double> row_lower;
+        std::vector<double> row_upper;
+
+        std::vector<HighsInt> Astart;     // size = num_col + 1
+        std::vector<HighsInt> Aindex;  // row indices
+        std::vector<double> Avalue;  // values
+}
 
 class ImuOptimizer{
     
     private:
         Optimizer opt_;
         SampleIMU zero_;
-        std::vector<sampleTLA> seg_;
+        std::vector<SampleTLA> seg_;
         std::queue<NormTLA> q_;
         NormTLA mean_;
         std::vector<double> tla_;
