52936dfe79e6bfaf09aeb83cadf8ca0d73f7a601
jaejinkim121 <gisgen9@snu.ac.kr>
2026-02-01 16:41:22 +0900

    Add sync receiver for arduino from gui

--- sketch_sep17a/sketch_sep17a.ino
+++ sketch_sep17a/sketch_sep17a.ino
@@ -1,6 +1,7 @@
 #define USE_USBCON
 #include <ros.h>
 #include <std_msgs/Int16MultiArray.h>
+#include <std_msgs/Bool.h>
 #include <Thread.h>
 #include <ThreadController.h>
 
@@ -8,6 +9,8 @@ ros::NodeHandle nh;
 bool flagSwitch = true;
 int pinIn = A0;
 int pinIn2 = A1;
+int pinOut = 0;
+int cur_sync = LOW;
 int valA = 0;
 int valB = 0;
 int valArray[20] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
@@ -16,9 +19,17 @@ std_msgs::Int16MultiArray msg_val;
 std_msgs::Int16MultiArray msg_sync;
 ros::Publisher pub_val("/afo_arduino/analog_val", &msg_val);
 ros::Publisher pub_sync("/afo_arduino/sync_val", &msg_sync);
+ros::Subscriber<std_msgs::Bool> sub_sync("/afo_gui/sync", &sync_cb);
 Thread receiveThread = Thread();
 Thread pubThread = Thread();
 ThreadController threadControl = ThreadController();
+
+void sync_cb(const std_msgs::Bool& msg){
+  if (cur_sync == HIGH) cur_sync = LOW;
+  else cur_sync = HIGH;
+  digitalWrite(pinOut, cur_sync);
+
+}
 
 void receiveSync(){
   
@@ -52,10 +63,11 @@ void pubMsg(){
 void setup(){
   pinMode(pinIn, INPUT);
   pinMode(pinIn2, INPUT);
+  pinMode(pinOut, OUTPUT);
   nh.initNode();
   nh.advertise(pub_val);
   nh.advertise(pub_sync);
-  
+  nh.subscribe(sub_sync);
   msg_val.data_length = 20;
   msg_sync.data_length = 20;
   // Thread Setup
